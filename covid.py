1bd2515f6a6e4f63ea783d839ed60530bf5858c9
Tyler <tyler.daubitz@gmail.com>
2021-12-23 15:48:44 -0500

    adding covid API

--- covid.py
+++ covid.py
@@ -1,14 +1,14 @@
 import requests
 
-url = "https://covid-19-data.p.rapidapi.com/report/country/all"
+url = "https://covid-19-data.p.rapidapi.com/report/totals"
 
-querystring = {"date":"2020-04-01"}
+querystring = {"date":"2020-08-08"}
 
 headers = {
     'x-rapidapi-host': "covid-19-data.p.rapidapi.com",
     'x-rapidapi-key': "2f3de43532mshfdabba757fe2b87p106ddajsna8d5fd3d5639"
     }
 
-response = requests.request("GET", url, headers=headers, params=querystring)
+response = requests.request("GET", url, headers=headers)
 
-print(response.text)
+print(response.json())
